adf703fe15fd26e0bb6316227b093b6299967b71
Jeremy FROMINVILLE <j.e.fromin@gmail.com>
2019-11-12 16:00:17 +0100

    TP3

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,16 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiArt4 = FigletFont.convertOneLine("Hello World!");     
+	// récupération du titre depuis le fichier de propriété.
+        String titre = ResourceBundle.getBundle("application").getString("titre");
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
+	
+	// affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
+
+    	/*String asciiArt4 = FigletFont.convertOneLine("Hello World!");     
         System.out.println(asciiArt4);    
         /*System.out.println( "Hello World!" );*/
     }
